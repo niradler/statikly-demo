359b6cbae96d145f4989f12b2b5643fe81e78320
Nir Adler <niradler55@gmail.com>
2022-12-14 01:02:38 +0200

    update dep, adding delete form

--- src/db.js
+++ src/db.js
@@ -1,7 +1,10 @@
-const todos = [{ id: 1, title: "First note" }];
+let todos = [{ id: 1, title: "First note" }];
 
-const get = async (id) => todos.find(t => t.id === id)
-const all = async (id) => todos
-const add = async (title) => todos.push({ title, id: todos.length + 1 })
+const get = async (id) => todos.find((t) => t.id === id);
+const del = async (id) => {
+  todos = todos.filter((t) => t.id != id);
+};
+const all = async (id) => todos;
+const add = async (title) => todos.push({ title, id: todos.length + 1 });
 
-module.exports = { all, get, add }
+module.exports = { all, get, add, del };
